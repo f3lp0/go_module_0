bf28232514c953bc164d9898aadd9ab180ba2029
f3lp0 <f3lp0@hotmail.com>
2022-09-29 08:07:25 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/f3lp0/go_module_0
+
+go 1.19
---
+++ utils.go
@@ -0,0 +1,11 @@
+// no main because this will be an external module for others
+package utils
+
+import ( 
+	"fmt"
+)
+
+// Public function, 1sr capital letter 
+func HelloWorld(){
+	fmt.Println("Hello World from f3lp0")
+}
